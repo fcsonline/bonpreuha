aa71dbdb4b684cadc85c93ce2b647c747ed41e4f
Ferran Basora <fcsonline@gmail.com>
2025-01-10 09:35:06 +0100

    Some fixes

--- server.js
+++ server.js
@@ -6,14 +6,19 @@ const app = express()
 // Reusable sanitization function
 const sanitizeInput = (input) => {
     if (!input) return ''
-    return input.replace(/[^a-zA-Z0-9_-]/g, '')
+    return input.replace(/[^a-zA-Z0-9 _-]/g, '')
 }
 
+const stripAnsiCodes = str => str.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, '');
+
 app.get('/healthcheck', (req, res) => {
-    res.json({ ok: true })
+    const datetime = new Date()
+
+    res.json({ ok: true, time: datetime.toISOString() })
 })
 
 app.get('/add', (req, res) => {
+    console.log('Add handler...')
     const { product } = req.query
     if (!product) {
         return res.status(400).json({ error: 'Missing product query parameter' })
@@ -22,11 +27,15 @@ app.get('/add', (req, res) => {
     const sanitizedProduct = sanitizeInput(product)
     const command = `PRODUCT="${sanitizedProduct}" cypress run --e2e /e2e/cypress/bonpreu.cy.js`
 
+    console.log(`Adding '${sanitizedProduct}' to the BonPreu cart...`)
+
     exec(command, (err, stdout, stderr) => {
         if (err) {
             return res.status(500).json({ error: stderr })
         }
-        res.json({ message: stdout.trim() })
+        res.json({ message: stripAnsiCodes(stdout.trim()) })
+
+        console.log(`Added '${sanitizedProduct}' to the BonPreu cart`)
     })
 })
 
