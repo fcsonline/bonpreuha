99ad9e63d5ff4bcb45005087a26c79dd38f9b7e2
Ferran Basora <fcsonline@gmail.com>
2025-01-10 14:52:03 +0100

    More logs

--- server.js
+++ server.js
@@ -1,43 +1,51 @@
-const express = require('express')
-const { exec } = require('child_process')
+const express = require("express");
+const { exec } = require("child_process");
 
-const app = express()
+const app = express();
 
 // Reusable sanitization function
 const sanitizeInput = (input) => {
-    if (!input) return ''
-    return input.replace(/[^a-zA-Z0-9 _-]/g, '')
-}
+  if (!input) return "";
+  return input.replace(/[^a-zA-Z0-9 _-]/g, "");
+};
 
-const stripAnsiCodes = str => str.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, '');
+const stripAnsiCodes = (str) =>
+  str.replace(
+    /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g,
+    ""
+  );
 
-app.get('/healthcheck', (req, res) => {
-    const datetime = new Date()
+app.get("/healthcheck", (req, res) => {
+  const datetime = new Date();
 
-    res.json({ ok: true, time: datetime.toISOString() })
-})
+  res.json({ ok: true, time: datetime.toISOString() });
+});
 
-app.get('/add', (req, res) => {
-    console.log('Add handler...')
-    const { product } = req.query
-    if (!product) {
-        return res.status(400).json({ error: 'Missing product query parameter' })
+app.get("/add", (req, res) => {
+  console.log("Add handler...");
+  const { product } = req.query;
+  if (!product) {
+    return res.status(400).json({ error: "Missing product query parameter" });
+  }
+
+  const sanitizedProduct = sanitizeInput(product);
+  const command = `PRODUCT="${sanitizedProduct}" cypress run --e2e /app/cypress/bonpreu.cy.js`;
+
+  console.log(`Adding '${sanitizedProduct}' to the BonPreu cart...`);
+
+  exec(command, (err, stdout, stderr) => {
+    if (err) {
+      console.log(
+        `Error adding '${sanitizedProduct}' to the BonPreu cart: ${stderr}`
+      );
+
+      return res.status(500).json({ error: stderr });
     }
+    res.json({ message: stripAnsiCodes(stdout.trim()) });
 
-    const sanitizedProduct = sanitizeInput(product)
-    const command = `PRODUCT="${sanitizedProduct}" cypress run --e2e /app/cypress/bonpreu.cy.js`
-
-    console.log(`Adding '${sanitizedProduct}' to the BonPreu cart...`)
-
-    exec(command, (err, stdout, stderr) => {
-        if (err) {
-            return res.status(500).json({ error: stderr })
-        }
-        res.json({ message: stripAnsiCodes(stdout.trim()) })
-
-        console.log(`Added '${sanitizedProduct}' to the BonPreu cart`)
-    })
-})
+    console.log(`Added '${sanitizedProduct}' to the BonPreu cart`);
+  });
+});
 
 // app.get('/remove', (req, res) => {
 //     const { product } = req.query
@@ -55,5 +63,9 @@ app.get('/add', (req, res) => {
 // })
 
 app.listen(3000, () => {
-    console.log('Server running on http://localhost:3000')
-})
+  if (!process.env.USERNAME || !process.env.PASSWORD) {
+    throw new Error("Missing required information: USERNAME or PASSWORD");
+  }
+
+  console.log("Server running on http://localhost:3000");
+});
