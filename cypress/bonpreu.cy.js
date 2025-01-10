2b0c4f5266ac0de36d78d5b495ae0b0bb6643f0d
Ferran Basora <fcsonline@gmail.com>
2025-01-10 13:37:47 +0100

    Fix get path

--- cypress/bonpreu.cy.js
+++ cypress/bonpreu.cy.js
@@ -1,59 +1,60 @@
-Cypress.on('uncaught:exception', (err, runnable) => {
+Cypress.on("uncaught:exception", (err, runnable) => {
   // returning false here prevents Cypress from
   // failing the test
-  return false
-})
+  return false;
+});
 
-const username = Cypress.env('USERNAME')
-const password = Cypress.env('PASSWORD')
-const product = Cypress.env('PRODUCT')
+const username = Cypress.env("USERNAME");
+const password = Cypress.env("PASSWORD");
+const product = Cypress.env("PRODUCT");
 
 // NOTE: Review this cypress bug https://github.com/cypress-io/cypress/issues/25373
-describe('Add product Bonpreu Online Store', () => {
-  it('Scrapes product information', () => {
+describe("Add product Bonpreu Online Store", () => {
+  it("Scrapes product information", () => {
     if (!username || !password || !product) {
-      throw new Error('Missing required information')
+      throw new Error("Missing required information");
     }
 
-    cy.visit('/')
+    cy.visit("/");
 
-    cy.get('#onetrust-accept-btn-handler').click() // Accept cookies
+    cy.get("#onetrust-accept-btn-handler").click(); // Accept cookies
 
-    cy.intercept('POST', '/recaptcha/api/siteverify', { success: true })
+    cy.intercept("POST", "/recaptcha/api/siteverify", { success: true });
 
-    cy.get('a[href="/login"]').first().click()
+    cy.get('main a[href="/login"]').first().click();
 
-    cy.origin('https://app.bonpreu.cat', () => {
-      const username = Cypress.env('USERNAME')
-      const password = Cypress.env('PASSWORD')
+    cy.origin("https://app.bonpreu.cat", () => {
+      const username = Cypress.env("USERNAME");
+      const password = Cypress.env("PASSWORD");
 
-      Cypress.on('uncaught:exception', (err, runnable) => {
-        return false
-      })
-
-      cy.visit('/openid-connect-server-webapp/login?lang=ca-ES&channel=osp', {
-        headers: {
-          'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
-        }
-      }) // Navigate to the login page
-
-      cy.window().then((win) => {
-        expect(win.grecaptcha).to.exist // Ensure reCAPTCHA is loaded
+      Cypress.on("uncaught:exception", (err, runnable) => {
+        return false;
       });
 
-      cy.get('input[name="username"]').type(username)
-      cy.get('input[name="password"]').type(password)
-      cy.get('input[type="submit"]').click()
-    })
+      cy.visit("/openid-connect-server-webapp/login?lang=ca-ES&channel=osp", {
+        headers: {
+          "user-agent":
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+        },
+      }); // Navigate to the login page
 
-    cy.visit('/') // Navigate
+      cy.window().then((win) => {
+        expect(win.grecaptcha).to.exist; // Ensure reCAPTCHA is loaded
+      });
+
+      cy.get('input[name="username"]').type(username);
+      cy.get('input[name="password"]').type(password);
+      cy.get('input[type="submit"]').click();
+    });
+
+    cy.visit("/"); // Navigate
     cy.get('form[role="search"]').within(() => {
-      cy.wait(2000)
-      cy.get('input[name="q"]').type(product) // Search product
-      cy.wait(3000)
-      cy.get('button[type="submit"]').click()
-    })
+      cy.wait(2000);
+      cy.get('input[name="q"]').type(product); // Search product
+      cy.wait(3000);
+      cy.get('button[type="submit"]').click();
+    });
 
-    cy.contains('Afegeix').first().scrollIntoView().click() // Add product
-  })
-})
+    cy.contains("Afegeix").first().scrollIntoView().click(); // Add product
+  });
+});
